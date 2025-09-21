a16a4e6969591a3af3f43f1bfbeb9973a793d78b
nakul-krishnakumar <nakulkrishnakumar86@gmail.com>
2025-09-21 22:15:20 +0530

    feat: add serial implementation of cross correlation

---
+++ include/xcorr_serial.hpp
@@ -0,0 +1,7 @@
+#ifndef CROSS_CORRELATION_SERIAL_H
+#define CROSS_CORRELATION_SERIAL_H
+
+#include <opencv2/opencv.hpp>
+cv::Mat crossCorrelationSerial(const cv::Mat &image, const cv::Mat &templ);
+
+#endif
